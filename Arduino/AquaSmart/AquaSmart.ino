93a9e755f46d75171f37dbe32263e4f0ff3c4234
Ivan Sapozhnik <ivan.sapozhnik@sixt.com>
2017-08-31 23:09:15 +0200

    GPIO to PIN mapping added

--- Arduino/AquaSmart/AquaSmart.ino
+++ Arduino/AquaSmart/AquaSmart.ino
@@ -1,9 +1,27 @@
 #include <AButton.h>
 #include <AquaSmartGUI.h>
 
+/*
+
+NODE MCU PIN GPIO MAPPING
+
+static const uint8_t D0   = 16;
+static const uint8_t D1   = 5;
+static const uint8_t D2   = 4;
+static const uint8_t D3   = 0;
+static const uint8_t D4   = 2;
+static const uint8_t D5   = 14;
+static const uint8_t D6   = 12;
+static const uint8_t D7   = 13;
+static const uint8_t D8   = 15;
+static const uint8_t D9   = 3;
+static const uint8_t D10  = 1;
+
+*/
+
 const int MENU_BUTTON = 2;
 const int MENU_ITEM_BUTTON = 3;
-const int FAN = 4;
+const int FAN = 0;
 const int LIGHT = 5;
 
 AquaSmartGUI gui;
